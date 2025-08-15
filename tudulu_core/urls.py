ab1ea97b345366fffaacc3183255abe8bb4cb1ac
Motoy Asaph Musan <66707555+motoy-asaph-musan@users.noreply.github.com>
2025-08-15 16:47:16 +0300

    Add files via upload
    Signed-off-by: Motoy Asaph Musan <66707555+motoy-asaph-musan@users.noreply.github.com>

--- tudulu_core/urls.py
+++ tudulu_core/urls.py
@@ -6,14 +6,30 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 # from users.views import home
 from equipment import views as equipment_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Home page
     path('', equipment_views.home, name='home'),
-    path('', auth_views.LoginView.as_view(), name='login'),
-    path('users/', include('users.urls')),
-    # path('equipment/', include('equipment.urls')),
+
+    # Login page
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+
+    # User-related URLs
+    # path('users/', include('users.urls')),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
+
+
+    # Equipment URLs with namespace
     path('equipment/', include(('equipment.urls', 'equipment'), namespace='equipment')),
-    path('', include('equipment.urls')),
+
+    # Default Django auth URLs (password reset, etc.)
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
